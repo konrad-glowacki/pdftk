1b9773aa21ae03beac3203e86b405412622dcaf9
Konrad Głowacki <konrad.glowacki@infakt.pl>
2013-01-30 11:39:38 +0100

    Repair

--- lib/pdftk.rb
+++ lib/pdftk.rb
@@ -1,9 +1,22 @@
+require 'logger'
+require 'digest/md5'
+require 'rbconfig'
+require RbConfig::CONFIG['target_os'] == 'mingw32' && !(RUBY_VERSION =~ /1.9/) ? 'win32/open3' : 'open3'
+require 'active_support/core_ext/class/attribute_accessors'
+
+begin
+  require 'active_support/core_ext/object/blank'
+rescue LoadError
+  require 'active_support/core_ext/blank'
+end
+
 class Pdftk
   EXE_NAME = "pdftk"
   @@config = {}
   cattr_accessor :config
 
   def initialize(source_array, destination_path)
+    @exe_path = find_binary_path
     @source_files = source_array
     @merged_file_path =  destination_path
     raise "Location of #{EXE_PATH} unknow" if @exe_path.empty?
@@ -22,4 +35,13 @@ class Pdftk
   rescue Exception => e
     raise "Failed to execute:\n#{command}\nError: #{e}"
   end
+
+  private
+
+    def find_binary_path
+      possible_locations = (ENV['PATH'].split(':')+%w[/usr/bin /usr/local/bin ~/bin]).uniq
+      exe_path ||= Pdftk.config[:exe_path] unless Pdftk.config.empty?
+      exe_path ||= possible_locations.map{|l| File.expand_path("#{l}/#{EXE_NAME}") }.find{|location| File.exists? location}
+      exe_path || ''
+    end
 end
